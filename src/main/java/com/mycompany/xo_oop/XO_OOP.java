1e2ebdd4ddad626c75b280c8121c65269bbcfa8c
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-07-25 20:34:23 +0700

    🔥 crate project

---
+++ src/main/java/com/mycompany/xo_oop/XO_OOP.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.xo_oop;
+
+/**
+ *
+ * @author USER
+ */
+public class XO_OOP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
