c485e02a2678222555ed50e7548b733cd0ccce3c
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-07-26 10:43:15 +0700

    create func startGame()

--- src/main/java/com/mycompany/xo_oop/Game.java
+++ src/main/java/com/mycompany/xo_oop/Game.java
@@ -4,18 +4,40 @@
  */
 package com.mycompany.xo_oop;
 
+import java.util.Scanner;
+
 /**
  *
  * @author USER
  */
 public class Game {
 
+    Player player1 = new Player("X", 0, 0, 0);
+    Player player2 = new Player("O", 0, 0, 0);
+    boolean isEnd = false;
+
+    public void startGame() {
+        System.out.println("Welcome to XO Game");
+        System.out.print("Start XO Games? (Y/N) :");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next().toLowerCase();
+        while (!input.equals("y") && !input.equals("n")) {
+            System.out.println("Welcome to XO Game");
+            System.out.print("Start XO Games? (Y/N) :");
+            input = sc.next().toLowerCase();
+        }
+        if (input.equals("n")) {
+            isEnd = true;
+        } else {
+            isEnd = false;
+        }
+    }
+
+    public void process() {
+    }
+
     public static void main(String[] args) {
-        Player player1 = new Player("X", 0, 0, 0);
-        Player player2 = new Player("O", 0, 0, 0);
-        String[][] board = {{"-", "-", "-"}, {"-", "-", "-"}, {"-", "-", "-"}};
-        Table table = new Table(board, player1, player1, player2);
-        table.printTable();
+
     }
 
 }
