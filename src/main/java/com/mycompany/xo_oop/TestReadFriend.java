50c8c41cb29aa186876291a72a83ade13abb12c8
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-08-08 23:21:37 +0700

    add try catch to class

--- src/main/java/com/mycompany/xo_oop/TestReadFriend.java
+++ src/main/java/com/mycompany/xo_oop/TestReadFriend.java
@@ -9,6 +9,8 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -16,17 +18,34 @@ import java.io.ObjectInputStream;
  */
 public class TestReadFriend {
 
-    public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
-        File file = new File("data.dat");
-        FileInputStream fis = new FileInputStream(file);
-        ObjectInputStream ois = new ObjectInputStream(fis);
-        Friend f1 = (Friend) ois.readObject();
-        Friend f2 = (Friend) ois.readObject();
-        System.out.println(f1.toString());
-        System.out.println(f2.toString());
+    public static void main(String[] args) {
+        FileInputStream fis = null;
+        try {
+            File file = new File("data1.dat");
+            fis = new FileInputStream(file);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            Friend f1 = (Friend) ois.readObject();
+            Friend f2 = (Friend) ois.readObject();
+            System.out.println(f1.toString());
+            System.out.println(f2.toString());
+            ois.close();
+            fis.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(TestReadFriend.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(TestReadFriend.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(TestReadFriend.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                if (fis != null) {
+                    fis.close();
 
-        ois.close();
-        fis.close();
+                }
+            } catch (IOException ex) {
+                Logger.getLogger(TestReadFriend.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
 
     }
 
